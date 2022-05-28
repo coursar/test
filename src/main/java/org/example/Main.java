cc24e8482ce13fc92e293fade923312e3aa9131a
student <student@localhost>
2022-05-28 19:05:56 +0300

    changed message, fixes #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello home, classroom");
+    System.out.println("hello from home, classroom");
   }
 }
